28e408cb2ccca25b16c4029e46484e12a2c398fa
Justin <justinjones38@myyahoo.com>
2026-04-28 20:20:32 -0500

    Completed lesson-05

--- src/exercises/lesson-05/SnackForm.jsx
+++ src/exercises/lesson-05/SnackForm.jsx
@@ -41,6 +41,7 @@ export default function SnackForm({
     }
   }
 
+  // Arrow function to validate name and rating
   const validateName = (name) => name.trim() !== '';
   const validateRating = (rating) => rating !== '';
 
