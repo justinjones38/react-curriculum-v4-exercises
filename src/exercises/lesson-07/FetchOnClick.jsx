7440e0c24a8786f77600e99a8266abaece1def73
Justin <justinjones38@myyahoo.com>
2026-05-12 23:37:21 -0500

    Completed lesson-07

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,14 +1,48 @@
 import './Lesson07Styles.css';
+import { getSinglePost } from './api';
+import { useState } from 'react';
 
 export default function FetchOnClick() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const randNum = Math.ceil(Math.random() * 100);
+      const info = await getSinglePost(randNum);
+      if (!info) {
+        throw new Error('Data cannot be fetched');
+      }
+      setData(info);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
-      <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
-      </div>
+      <button type="button" onClick={fetchData}>
+        Get post
+      </button>
+      {!error ? (
+        <div className="content">
+          {!loading ? (
+            <>
+              <h2>{data.title}</h2>
+              <p>{data.body}</p>
+            </>
+          ) : (
+            <h2>Loading...</h2>
+          )}
+        </div>
+      ) : (
+        <h2>Cannot fetch data</h2>
+      )}
     </div>
   );
 }
--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
 import './Lesson07Styles.css';
+import { getPosts } from './api';
 
 export default function FetchOnRender() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const info = await getPosts();
+        if (!info) {
+          throw new Error('Cannot fetch data');
+        }
+        setData(info);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
-      <div className="content">
-        TODO: Replace me with fetched data when the component renders
-      </div>
+      {!error ? (
+        <div className="content">
+          {!loading ? (
+            data.map((item) => (
+              <div key={item.id}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+              </div>
+            ))
+          ) : (
+            <h2>Loading</h2>
+          )}
+        </div>
+      ) : (
+        <h2>Cannot fetch data</h2>
+      )}
     </div>
   );
 }
