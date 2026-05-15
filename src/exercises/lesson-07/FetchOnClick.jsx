dfcd0bb401d9767e2fab110012c87c997d5d4cf2
Justin <justinjones38@myyahoo.com>
2026-05-14 22:42:51 -0500

    completed lesson-07

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
+import { getSinglePost } from './api';
 import './Lesson07Styles.css';
 
 export default function FetchOnClick() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const randomNum = Math.ceil(Math.random() * 100);
+
+  const handleChange = async (num) => {
+    setLoading(true);
+    try {
+      const resData = await getSinglePost(num);
+      if (!resData) {
+        throw new Error('Error receiving response data');
+      }
+      setData(resData);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
+      <button type="button" onClick={() => handleChange(randomNum)}>
+        Get post
+      </button>
       <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
+        {error ? (
+          <h2>Cannot fetch data </h2>
+        ) : !loading ? (
+          <div>
+            <h2>{data.title}</h2>
+            <p>{data.body}</p>
+          </div>
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </div>
     </div>
   );
--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,20 @@
 import './Lesson07Styles.css';
-
+import { getPosts } from './api';
 export default function FetchOnRender() {
+  const { data, error, loading } = getPosts();
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {loading ? <h2>Loading</h2> : null}
+        {error ? <h2>Cannot fetch data </h2> : null}
+        {data.map((item) => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
