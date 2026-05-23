7453b9fd378a55624d4b76317cdc60c3ec8b6f6c
Justin <justinjones38@myyahoo.com>
2026-05-23 08:48:54 -0500

    Corrected fetchOnRender

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -7,22 +7,18 @@ export default function FetchOnRender() {
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
-      {loading ? (
-        <h2>Loading </h2>
-      ) : (
-        <div className="content">
-          {error ? (
-            <h2>Cannot fetch data</h2>
-          ) : (
-            data.map((item) => (
-              <div key={item.id}>
+      <div className="content">
+        {loading ? <h2>Loading ...</h2> : null}
+        {error ? <h2>Cannot fetch data</h2> : null}
+        {!loading && !error && data
+          ? data.map((item) => (
+              <div className="container" key={item.id}>
                 <h2>{item.title}</h2>
                 <p>{item.body}</p>
               </div>
             ))
-          )}
-        </div>
-      )}
+          : null}
+      </div>
     </div>
   );
 }
